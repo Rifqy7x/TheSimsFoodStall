d4275ebaa4a7d9f94440e929d5c9b1c1942ade29
Rifqy <mrifqyaa@gmail.com>
2018-12-03 09:53:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rifqy/thesimsfoodstall/CryptoActivity.java
@@ -0,0 +1,13 @@
+package com.example.rifqy.thesimsfoodstall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CryptoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto);
+    }
+}
